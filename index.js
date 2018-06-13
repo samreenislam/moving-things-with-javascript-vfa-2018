56eb22beec36cc89e65da667672dff41af72358b
Samreen Islam <samreen.sfi@gmail.com>
2018-06-13 22:33:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const app = "I don't do much."
 var dodger = document.getElementById('dodger')
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
